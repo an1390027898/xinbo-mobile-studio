4fdc3cf2003fb37dd371a7b7d4857d4a3e75a28d
安宁pro <mac@anningdeMacBook-Pro.local>
2026-07-26 13:42:56 +0800

    改进 iPhone 两阶段强制更新流程

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='xinbo-pwa-v17-force-update-entry'
+const VERSION='xinbo-pwa-v18-two-stage-update'
 const CORE=['./','./index.html','./update.html','./knowledge.html','./preferences.html','./output.html','./license.html','./splash.html','./mobile-full.css','./mobile-adapter.js','./manifest.webmanifest','./icon-512.png','./video-final.css','./video-final.js','./lame.min.js','./vendor/lunar-javascript/lunar.js']
 self.addEventListener('install',event=>event.waitUntil(caches.open(VERSION).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())))
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==VERSION).map(key=>caches.delete(key)))).then(()=>self.clients.claim())))
